b29e1b7866a7d5bfd8b3f52fc8d6c3289daaa83a
Klassischer <103065492+Klassischer@users.noreply.github.com>
2024-10-31 17:37:13 +0100

    wrote code for second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+//Write a function that simply repeats the string a given number of times:
+// repeatString('hey', 3) // returns 'heyheyhey'
 
-};
+
+
+const repeatString = function(string,num) {
+    let result = "";
+    for (let i = 1; i <= num; i++){
+
+        result += string;
+    }
+    return result;
+ };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
