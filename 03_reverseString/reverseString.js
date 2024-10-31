c4e7a21428d14ed5e3cd2866b882e0040109df90
Klassischer <103065492+Klassischer@users.noreply.github.com>
2024-10-31 17:57:16 +0100

    coded the third exercise and checked all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = string.split(""); //split into an array
+    newString.reverse();  //reverse the array
+    return newString.join("");  //join the array back into a string
 };
 
 // Do not edit below this line
