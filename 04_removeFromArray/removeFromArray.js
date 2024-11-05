38e9facf1f58d9219cd931e27d12ccb14383b139
Klassischer <103065492+Klassischer@users.noreply.github.com>
2024-11-05 17:08:34 +0100

    finished exercise 4 and added tests plus redoing the exercise after learning about doing rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+/*1. create an empty array and an empty variable for the second argument
+2. check if the second argument is part of the array
+3. if so, at which position does it appear?
+4. check if it appears more than one time
+5. remove all the instances of the argument in the array (maybe with a loop in case there are several instances)
+6. return the new array*/
+
+
+const removeFromArray = function(array, ...element) {
+   for (let i = 0; i < element.length; i++) {
+    let elementItem = element[i];
+    if (array.includes(elementItem)) {
+        let position = array.indexOf(elementItem);
+        while ( position !== -1) {
+            array.splice(position, 1);
+            position = array.indexOf(elementItem);
+        }
+        
+    }
+    else {
+        console.log(`${element} does not exist in the array`);
+      }
+    }
+    return array;
 };
 
 // Do not edit below this line
