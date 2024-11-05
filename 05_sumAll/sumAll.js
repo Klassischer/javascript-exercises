54b43df50e1dc8de9eca72499271e55a864e8792
Klassischer <103065492+Klassischer@users.noreply.github.com>
2024-11-05 17:33:17 +0100

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {  
+    let sum = 0;
+    if (int1 < 1 || int2 < 1 || !Number.isInteger(int1) || !Number.isInteger(int2)){
+        return "ERROR";
+    }
+    //detemine which number is bigger
+    let start = Math.min(int1, int2);
+    let end = Math.max(int1, int2);
+    //loop through the numbers
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
+    
 
 };
 
